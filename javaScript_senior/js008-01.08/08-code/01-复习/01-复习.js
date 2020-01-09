fc3a0663712148a1e7f1a7f250362191bf0180f2
zl21 <44433851+zl21@users.noreply.github.com>
2020-01-09 10:27:38 +0800

    008_all.

---
+++ javaScript_senior/js008-01.08/08-code/01-复习/01-复习.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * 
+ * 流程控制:代码的执行顺序
+ * 流程控制:顺序结构,分支结构,循环结构
+ * 顺序机构: 代码从上到下执行的顺序
+ * 分支结构: if,if-else, if else-if else-if else ,switch-case 三元表达式
+ * 循环结构: while,do-while,for,for-in,for-of
+ * 
+ * 
+ * 
+ * 
+ */
